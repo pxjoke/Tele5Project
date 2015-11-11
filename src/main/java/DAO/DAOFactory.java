ab3ea17b718eadf552d4c3981ee7528a3b93c40d
pxjoke <pxjoke@gmail.com>
2015-11-11 22:02:25 +0300

    maven support added

---
+++ src/main/java/DAO/DAOFactory.java
@@ -0,0 +1,8 @@
+package DAO;
+
+/**
+ * Created by pxjok on 11.11.2015.
+ */
+public interface DAOFactory {
+
+}
---
+++ src/main/java/DAO/beans/User.java
@@ -0,0 +1,8 @@
+package DAO.beans;
+
+/**
+ * Created by pxjok on 11.11.2015.
+ */
+public class User {
+
+}
