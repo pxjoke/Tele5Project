121841ccd46369244e7cfc27b69b867a0a40c02f
pxjoke <pxjoke@gmail.com>
2015-11-18 01:19:06 +0300

    Operation bean added

---
+++ src/main/java/controllers/DAO/beans/Operation.java
@@ -0,0 +1,70 @@
+package controllers.DAO.beans;
+
+/**
+ * Created by pxjok on 18.11.2015.
+ */
+public class Operation {
+    private int id;
+    private int accountId;
+    private String type;
+    private double cost;
+    private int amount;
+    private String time;
+    private int serviceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+}
